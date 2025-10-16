cf28ff4bb4c83826ff4df5d8fb69575778927145
shifa772000 <shifa68978@gmail.com>
2025-10-16 15:38:42 +0400

    Delete src directory

--- src/validations/EmailValidation.js
+++
@@ -1,7 +0,0 @@
-import * as yup from "yup";
-
-const EmailValidation = yup.object().shape({
-  email: yup.string().email("Invalid email format").required("Email is required"),
-});
-
-export default EmailValidation;
